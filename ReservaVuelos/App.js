cfd6b2436512f7a0684e76ee93284f08c205ab43
Ch4s3d <ramozchris@gmail.com>
2022-09-01 16:32:41 -0500

    refactor: change syntax for DeepSource (2)

--- ReservaVuelos/App.js
+++ ReservaVuelos/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-
 import Appstyles from './components/GeneralStyles/Styles.sass';
 import MainStack from './components/MainStack';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
